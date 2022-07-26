0744af108bc6e6ca7cca737190eb2a12689df022
Jefferson200225 <110010433+Jefferson200225@users.noreply.github.com>
2022-07-26 20:33:34 +0530

    Second Commit

--- Main.py
+++ Main.py
@@ -5,44 +5,35 @@ class Stack:
         self.size = size
 
     def is_empty(self):
-        # Write code here
-        if self.top == -1 :
-            return 1
-        else :
-            return 0
+          if self.top == -1 :
+           return 1
+        else:
+           return 0
 
 
     def is_full(self):
-        # Write code here
-         if self.top == (self.size - 1) :
-                return 1
-         else :
-                return 0
+        if (self.top == self.size-1):
+          return 1
+       else:
+          return 0
+    
         
 
     def push(self, data):
-        if not self.is_full():
-           self.top = self.top+1
-          self.l[self.top]=y
-         else:
-            print("The stack is full")
-          return "Push is not allowed"
+        if not self.is_full()==1:
+            self.top=self.top+1
+            self.items[self.top]=data
         
 
     def pop(self):
-        if not self.is_empty():
-             print(self.l[self.top])
-          self.top = self.top-1
-          return 0
-        else:
-             return "the stack is empty"
+       if not self.is_empty()==1:
+            self.top = self.top-1
         
 
 
     def status(self):
-        for x in range(self.top+1):
-          print(self.l[x])
-        # Write code here
+         for x in range(self.top+1):
+          print(self.items[x])
 
 # Do not change the following code
 size, queries = map(int, input().rstrip().split())
