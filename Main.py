9395d006083e3e4cf088721773196250c59877a6
Jefferson200225 <110010433+Jefferson200225@users.noreply.github.com>
2022-07-26 20:38:58 +0530

    Second commit

--- Main.py
+++ Main.py
@@ -1,38 +1,36 @@
-import os
 class Stack:
-    def __init__(self, size):
-        self.items = []
+    def _init_(self, size):
+        self.items = [None]*size
         self.size = size
+        self.top = -1
+       
 
     def is_empty(self):
-          if self.top == -1 :
+        if self.top == -1 :
            return 1
         else:
            return 0
 
 
     def is_full(self):
-        if (self.top == self.size-1):
+       if (self.top == self.size-1):
           return 1
        else:
           return 0
-    
-        
 
     def push(self, data):
         if not self.is_full()==1:
             self.top=self.top+1
             self.items[self.top]=data
-        
 
     def pop(self):
-       if not self.is_empty()==1:
+        if not self.is_empty()==1:
+            # Write code here
             self.top = self.top-1
-        
-
 
     def status(self):
-         for x in range(self.top+1):
+        # Write code here
+        for x in range(self.top+1):
           print(self.items[x])
 
 # Do not change the following code
